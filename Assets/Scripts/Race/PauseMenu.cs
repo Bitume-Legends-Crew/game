8755380e0c56a7ceb0ee4e39b397007befa23ba2
Xavier2p <83869794+Xavier2p@users.noreply.github.com>
2022-05-04 10:02:01 +0200

    Add Pause Manu, and Lightning

--- Assets/Scripts/Race/PauseMenu.cs
+++ Assets/Scripts/Race/PauseMenu.cs
@@ -44,5 +44,7 @@ public class PauseMenu : MonoBehaviour
 
     public void Retry()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
     }
 }
